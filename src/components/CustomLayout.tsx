a8c7a2f5ba4a1c33a66a6d181822b1c9d5197eb4
joaocansi <whitshs2003@gmail.com>
2024-09-22 20:42:16 -0300

    feat: add

--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-nested-ternary */
 
@@ -10,52 +11,116 @@ import {
   Toolbar,
   List,
   ListItem,
-  ListItemText,
   Box,
   IconButton,
   Collapse,
+  Button,
+  Typography,
 } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
 import Link from 'next/link'
 import { grey } from '@mui/material/colors'
 import { MarkdownMetadata, useData } from '@/hooks/use-data'
+import { usePathname } from 'next/navigation'
+
 import Branding from './Branding'
 
 type CustomNavigation = {
   title: string
+  segment: string
   children?: CustomNavigation[]
 }
 
 const fixNavigationSegment = (
   markdownMetadatas: MarkdownMetadata[]
 ): CustomNavigation[] =>
-  markdownMetadatas.map(({ title, children }) => ({
+  markdownMetadatas.map(({ title, segment, children }) => ({
     title,
+    segment,
     children: children.length ? fixNavigationSegment(children) : undefined,
   }))
 
-const NestedList: React.FC<{ items: CustomNavigation[] }> = ({ items }) => (
+const NestedList: React.FC<{
+  items: CustomNavigation[]
+  prevSegment: string
+  pathname: string
+}> = ({ items, prevSegment = '', pathname }) => (
   <List>
     {items.map((item) => (
-      <React.Fragment key={item.title}>
-        <ListItem component={Link} href="">
-          <ListItemText primary={item.title} sx={{ marginLeft: 1 }} />
-        </ListItem>
-        {item.children && item.children.length > 0 && (
-          <List component="div" disablePadding>
-            <Box sx={{ pl: 4 }}>
-              <NestedList items={item.children} />
-            </Box>
-          </List>
-        )}
-      </React.Fragment>
+      <NestedListItem
+        key={item.title}
+        item={item}
+        prevSegment={prevSegment}
+        pathname={pathname}
+      />
     ))}
   </List>
 )
 
+interface NestedListItemProps {
+  item: CustomNavigation
+  prevSegment: string
+  pathname: string
+}
+
+const NestedListItem: React.FC<NestedListItemProps> = ({
+  item,
+  prevSegment,
+  pathname,
+}) => {
+  const href = `${prevSegment}/${item.segment}`
+  const isActive = href === pathname
+
+  return (
+    <React.Fragment key={item.title}>
+      <ListItem>
+        {item.children && item.children.length > 0 ? (
+          <Typography variant="body1" sx={{ pl: 1.5 }}>
+            {item.title}
+          </Typography>
+        ) : (
+          <Button
+            component={Link}
+            href={href}
+            variant="text"
+            size="large"
+            sx={{
+              width: '100%',
+              justifyContent: 'flex-start',
+              color: isActive ? 'primary.main' : 'text.primary',
+              '&:hover': {
+                backgroundColor: 'transparent',
+              },
+            }}
+          >
+            {item.title}
+          </Button>
+        )}
+      </ListItem>
+      {item.children && item.children.length > 0 && (
+        <List component="div" disablePadding>
+          <Box sx={{ pl: 2 }}>
+            {item.children.map((child) => (
+              <NestedListItem
+                key={child.title}
+                item={child}
+                prevSegment={`${prevSegment}/${item.segment}`}
+                pathname={pathname}
+              />
+            ))}
+          </Box>
+        </List>
+      )}
+    </React.Fragment>
+  )
+}
+
 const TestLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [open, setOpen] = useState(false)
+
+  const pathname = usePathname()
+
   const markdownMetadatas = fixNavigationSegment(useData().markdownMetadatas)
 
   return (
@@ -93,7 +158,11 @@ const TestLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
           }}
         >
           <Collapse orientation="vertical" in={open}>
-            <NestedList items={markdownMetadatas} />
+            <NestedList
+              pathname={pathname}
+              items={markdownMetadatas}
+              prevSegment=""
+            />
           </Collapse>
         </Box>
       </Box>
